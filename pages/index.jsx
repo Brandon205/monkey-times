d504eca1d94fde234ceaffa01af8ea1edee40f9e
Brandon Black <53108034+Brandon205@users.noreply.github.com>
2023-01-12 09:35:57 -0800

    Scramble generator working

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,25 @@
+import { useEffect, useState } from 'react';
+import { randomScrambleForEvent } from 'cubing/scramble';
+
+import { HiRefresh } from 'react-icons/hi';
+
 export default function Home() {
+  const [scramble, setScramble] = useState("");
+  const [event, setEvent] = useState('222');
+
+  useEffect( () => {
+    generateScramble();
+  }, [])
+
+  let generateScramble = async () => {
+    const scram = await randomScrambleForEvent(event);
+    setScramble(scram.toString());
+  }
+
   return (
     <div className="flex justify-center h-screen items-center">
-      <h1 className="text-red-500 text-5xl">Hello world</h1>
+      <h1 className="text-yellow-400 text-5xl">{scramble}</h1>
+      <button onClick={generateScramble} className="p-5 text-white text-3xl"><HiRefresh /></button>
     </div>
   )
 }
